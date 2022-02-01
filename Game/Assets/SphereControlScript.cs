3264c1c4415d3a0293b50e0951df13b7cb3bc453
STUDENT Emily Switzer <Emily.Switzer@students.ittralee.ie>
2022-02-01 10:58:49 +0000

    Signed-off-by: STUDENT Emily Switzer <Emily.Switzer@students.ittralee.ie>

--- Game/Assets/SphereControlScript.cs
+++ Game/Assets/SphereControlScript.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class SphereControlScript : MonoBehaviour
 {
+    Renderer my_renderer;
+    bool is_selected = false;
+    IInteractable selected_object;
     // Start is called before the first frame update
     void Start()
     {
-        
+        my_renderer = GetComponent<Renderer>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        is_selected = !is_selected;
+
+        if (is_selected)
+            my_renderer.material.color = Color.yellow;
+        else
+            my_renderer.material.color = Color.white;
+
     }
 }
